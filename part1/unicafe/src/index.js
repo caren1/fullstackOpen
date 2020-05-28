01d8f5267f85f5b3d4b237f74f5a9238c7dfa445
caren1 <czarnocki.w@wp.pl>
2020-05-29 00:28:24 +0200

    1.10 - unicafe, step5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,81 +1,58 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = ({title}) => <div><h1>{title}</h1></div>;
+const Header = ({text}) => <div><h1>{text}</h1></div>;
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>;
 
-const Statistic = ({text, count}) => {
-  
-  if(text ==='positive percentage'){
-    return <p>{text} {count}%</p> 
-  }else{
-    return <p>{text} {count}</p>
-  }
-
-}
+const Statistic = ({text, count}) => { return text ==='positive percentage' ?  <p>{text} {count}%</p> : <p>{text} {count}</p> }
 
 const Statistics = ({good, neutral, bad}) => {
 
-  // const Statistics = ({counts}) => {
-  // const good = counts[0];
-  // const neutral = counts[1];
-  // const bad = counts[2];
-
-  const countAverage = function(a, b, c){
-    let total = a + b + c;
-    let average = ((a * 1) + (b * 0) + (c * -1)) / total;
+  const countAverage = function(good, neutral, bad){
+    let total = good + neutral + bad;
+    let average = ((good * 1) + (neutral * 0) + (bad * -1)) / total;
     return Math.floor(average);
   }
 
-  const countPercentage = function(a, b, c){
-    let total = a + b + c;
-    let percentage = (a/total) * 100;
+  const countPercentage = function(good, neutral, bad){
+    let total = good + neutral + bad;
+    let percentage = (good/total) * 100;
     return Math.floor(percentage);
   }
 
-
-  if(good === 0 && neutral === 0 && bad === 0){
-    return <p>No feedback given yet</p>
-  }else{
+  if(!(good || neutral || bad)) return <p>No feedback given yet</p>
+  
     return(
       <div>
-        <Statistic text={'good'} count={good} />
-        <Statistic text={'neutral'} count={neutral} />
-        <Statistic text={'bad'} count={bad} />
-        <Statistic text={'all'} count={good + neutral + bad} />
-    
-        <Statistic text={'average'} count={countAverage(good, neutral, bad)} />
-        <Statistic text={'positive percentage'} count={countPercentage(good, neutral, bad)} />
+        <Statistic text={'Good feedback:'} count={good} />
+        <Statistic text={'Neutral feedback:'} count={neutral} />
+        <Statistic text={'Bad feedback:'} count={bad} />
+        <Statistic text={'Total review count:'} count={good + neutral + bad} />
+        <Statistic text={'Average:'} count={countAverage(good, neutral, bad)} />
+        <Statistic text={'Positive percentage'} count={countPercentage(good, neutral, bad)} />
       </div>
       )
-  }
 }
   
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const feedbackTitle = 'Give Feedback please';
+  const statisticsTitle = 'Statistics:'
 
   return (
     <div>
-      <Header title={'give feedback'} />
+      <Header text={feedbackTitle} />
       <Button onClick={() => setGood(good + 1)} text={'good'} />
       <Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
       <Button onClick={() => setBad(bad + 1)} text={'bad'} />
       <hr />
-      <Header title={'statistics'} />
-      {/* <Statistic text={'good'} count={good} /> */}
-      {/* <Statistic text={'neutral'} count={neutral} /> */}
-      {/* <Statistic text={'bad'} count={bad} />  */}
-      {/* <Statistics counts={good, neutral, bad} /> */}
-      {/* whoops, actually done the next exercise without reading it :) */}
+      <Header text={statisticsTitle} />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
 
-ReactDOM.render(<App />, 
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
