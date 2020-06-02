d0d550398e4178fd1baf03da07b0ae965b9bfc52
caren1 <czarnocki.w@wp.pl>
2020-06-02 22:01:15 +0200

    2.11 - the phonebook, step6

--- part2/thephonebook/src/components/App.js
+++ part2/thephonebook/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './Filter'
 import PersonForm from './PersonForm'
@@ -6,16 +7,28 @@ import Persons from './Persons'
 
 const App = ({ props }) => {
 
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newPhoneNumber, setNewPhoneNumber ] = useState('')
   const [ showAll, setShowAll ] = useState('')
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data);
+    })
+  },[])
+
+  // const hook = () => {
+  //   axios
+  //   .get('http://localhost:3001/persons')
+  //   .then(response => {
+  //     setPersons(response.data)
+  //   })
+  // }
+  // useEffect(hook, []);
+  
 
   let personsToShow = showAll ? persons.filter(person => person.name.toLowerCase().includes(showAll.toLowerCase())) : persons
 
