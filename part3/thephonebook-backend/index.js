bb110c18695ede7218071370f84d7710fdacf006
caren1 <czarnocki.w@wp.pl>
2020-06-10 22:09:54 +0200

    phonebook backend, step5

--- part3/thephonebook-backend/index.js
+++ part3/thephonebook-backend/index.js
@@ -64,6 +64,27 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const id = Math.floor(Math.random() * (100 - persons.length)) + persons.length;
+    const body = request.body
+
+    if(!body.name){
+        return response.status(404).json({
+            error: 'content of request is missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id,
+    }
+
+    console.log(person)
+    persons = persons.concat(person);
+    response.json(person)
+})
+
 const port = 3001
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
