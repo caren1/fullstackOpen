ac1a70f6d7db04e7d1b7590b6e888ee0c8ace1e9
caren1 <czarnocki.w@wp.pl>
2020-06-22 16:14:51 +0200

    constructed app.js

--- part4/index.js
+++ part4/index.js
@@ -5,7 +5,7 @@
 // const mongoose = require('mongoose')
 
 
-// const app = require('./app') // <- actual Express application
+const app = require('./app') // <- actual Express application
 const http = require('http')
 const config = require('./utils/config')
 // const logger = require('./utils/logger')
