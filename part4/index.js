b74fc0ca81e974a304efe34f4c51173de0180eaa
caren1 <czarnocki.w@wp.pl>
2020-06-22 15:48:22 +0200

    Added utils/logger and utils/config

--- part4/index.js
+++ part4/index.js
@@ -1,8 +1,19 @@
-require('dotenv').config()
+const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+
+
+// const app = require('./app') // <- actual Express application
+// const http = require('http')
+// const config = require('./utils/config')
+// const logger = require('./utils/logger')
+// const server = http.createServer(app);
+
+// server.listen(config.PORT, () => {
+//     logger.info(`Server running on port ${config.PORT}`)
+// })
 
 const blogSchema = mongoose.Schema({
   title: String,
