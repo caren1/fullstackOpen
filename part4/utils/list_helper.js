aa605622b8d3382b9e1b14bba0e70976ffad0e42
caren1 <czarnocki.w@wp.pl>
2020-06-22 20:57:14 +0200

    4.3 - 4.4 - helper functions and unit tests, step1 and step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (posts) => {
+    const likes = posts.map(post => post.likes)
+
+    const reducer = (sum, item) => sum + item
+      
+
+    return likes.length === 0 ? 0 : likes.reduce(reducer, 0)
+}
+
+module.exports = { dummy, totalLikes }
