8d218eb19f4dd1b8f8f8b86cdbe46676ad591bb3
caren1 <czarnocki.w@wp.pl>
2020-06-22 22:14:27 +0200

    till 4.7* - helper fucntions and unit tests, step5 ; together with code pieces of @smallsco to compare the high level solution

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -1,10 +1,10 @@
 const listHelper = require('../utils/list_helper')
 
-// test('dummy returns one', () => {
-//   const blogs = []
-//   const result = listHelper.dummy(blogs)
-//   expect(result).toBe(1)
-// })
+test('dummy returns one', () => {
+  const blogs = []
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
 
 describe('total of likes', () => {
 
@@ -191,7 +191,6 @@ describe('the most favorite blog', () => {
             __v: 0
         }
     ]
-
 
 
     test('when list have one blog', () => {
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,6 +1,6 @@
-const dummy = (blogs) => {
-  return 1
-}
+/* Provided piece of code from github/@smallsco in order to analyse and compare the solutions */
+
+const dummy = blogs => 1
 
 const totalLikes = (posts) => {
     const likes = posts.map(post => post.likes)
@@ -8,7 +8,11 @@ const totalLikes = (posts) => {
     return likes.length === 0 ? 0 : likes.reduce(reducer, 0)
 }
 
-const favoriteBlog = (blogs) => {
+// const totalLikes = blogs => (
+//     blogs.reduce((sum, blog) => sum + blog.likes, 0)
+//   )
+
+const favouriteBlog = (blogs) => {
     const maxLiked = Math.max(...blogs.map(blog => blog.likes))
     if(maxLiked > 0) {
         let likes = blogs.map(blog => blog.likes)
@@ -17,6 +21,23 @@ const favoriteBlog = (blogs) => {
         return {}
     }
 }
+
+// Given a list of blogs, return the blog with the most likes.
+// const favouriteBlog = blogs => {
+//     let max = 0
+//     let favourite = null
+//     blogs.map((blog) => {
+//       if (blog.likes > max) {
+//         max = blog.likes
+//         favourite = {
+//           title: blog.title,
+//           author: blog.author,
+//           likes: blog.likes
+//         }
+//       }
+//     })
+//     return favourite
+//   }
 
 const mostBlogs = (blogs) => {
     const maxNumber = Math.max(...blogs.map(blog => blog.blogs))
@@ -28,4 +49,36 @@ const mostBlogs = (blogs) => {
     }
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
+// Given a list of blogs, return the author with the largest amount
+// const mostBlogs = blogs => {
+//     if (blogs.length === 0) return null
+//     const authorCounts = _.countBy(blogs, blog => blog.author)
+//     return _.reduce(authorCounts, (max, numBlogs, author) => {
+//       if (numBlogs > max.blogs) {
+//         max.blogs = numBlogs
+//         max.author = author
+//       }    
+//       return max
+//     }, {'blogs': 0, 'author': ''})
+//   }
+
+
+// Given a list of blogs, return the author whos blog posts have the most total
+// likes.
+// const mostLikes = blogs => {
+//     if (blogs.length === 0) return null
+//     const authorGroups = _.groupBy(blogs, blog => blog.author)
+//     const authors = Object.keys(authorGroups)
+//     for (const author of authors) {
+//       authorGroups[author] = totalLikes(authorGroups[author])
+//     }
+//     return _.reduce(authorGroups, (max, numLikes, author) => {
+//       if (numLikes > max.likes) {
+//         max.likes = numLikes
+//         max.author = author
+//       }    
+//       return max
+//     }, {'likes': 0, 'author': ''})
+//   }
+
+module.exports = { dummy, totalLikes, favouriteBlog, mostBlogs }
