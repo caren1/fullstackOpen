d76c548f4f9b21c45e6a116e9503df530efc14e5
caren1 <czarnocki.w@wp.pl>
2020-06-22 21:55:19 +0200

    4.4* - helper functions and unit tests, step2

--- part4/tests/dummy.test.js
+++ part4/tests/dummy.test.js
@@ -135,3 +135,165 @@ describe('total of likes', () => {
         expect(result).toBe(0)
     })
 })
+
+describe('the most favorite blog', () => {
+
+    const listWithOneBlog = [{
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }]
+
+    const listWithOneBlogAndNoLikes = [{
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 0,
+        __v: 0
+    }]
+
+    const listWithMultipleBlogs = [{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const listWithMultipleBlogsAndNoLikes = [{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 0,
+            __v: 0
+        }
+    ]
+
+
+
+    test('when list have one blog', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual(listWithOneBlog[0]);
+    })
+
+    test('when list have one blog, but 0 likes', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlogAndNoLikes)
+        expect(result).toEqual({});
+    })
+
+    test('when list have multiple blogs', () => {
+        const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+        expect(result).toEqual(listWithMultipleBlogs[1]);
+    })
+
+    test('when list have multiple blogs, but 0 likes', () => {
+        const result = listHelper.favoriteBlog(listWithMultipleBlogsAndNoLikes)
+        expect(result).toEqual({});
+    })
+})
+
+describe('the highest quantity of blogs', () => {
+    const listWithOneBlog = [{
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        blogs: 2,
+        __v: 0
+    }]
+
+    const listWithOneBlogAndNoBlogs = [{
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 0,
+        blogs: 0,
+        __v: 0
+    }]
+
+    const listWithMultipleBlogs = [{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            blogs: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 5,
+            blogs: 6,
+            __v: 0
+        }
+    ]
+
+    const listWithMultipleBlogsAndNoBlogs = [{
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            blogs: 0,
+            __v: 0
+        },
+        {
+            _id: "5a422bc61b54a676234d17fc",
+            title: "Type wars",
+            author: "Robert C. Martin",
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+            likes: 0,
+            blogs: 0,
+            __v: 0
+        }
+    ]
+
+
+    test('when list have one blog and contains blogs', () => {
+        const result = listHelper.mostBlogs(listWithOneBlog)
+        expect(result).toEqual(listWithOneBlog[0]);
+    })
+
+    test('when list have one blog and contains no blogs', () => {
+        const result = listHelper.mostBlogs(listWithOneBlogAndNoBlogs)
+        expect(result).toEqual({})
+    })
+
+    test('when list have multiple blogs and contains blogs', () => {
+        const result = listHelper.mostBlogs(listWithMultipleBlogs)
+        expect(result).toEqual(listWithMultipleBlogs[1])
+    })
+
+    test('when list have multiple blogs and contains no blogs', () => {
+        const result = listHelper.mostBlogs(listWithMultipleBlogsAndNoBlogs)
+        expect(result).toEqual({})
+    })
+})
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,11 +4,28 @@ const dummy = (blogs) => {
 
 const totalLikes = (posts) => {
     const likes = posts.map(post => post.likes)
-
     const reducer = (sum, item) => sum + item
-      
-
     return likes.length === 0 ? 0 : likes.reduce(reducer, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    const maxLiked = Math.max(...blogs.map(blog => blog.likes))
+    if(maxLiked > 0) {
+        let likes = blogs.map(blog => blog.likes)
+        return blogs[likes.indexOf(maxLiked)];
+    }else{
+        return {}
+    }
+}
+
+const mostBlogs = (blogs) => {
+    const maxNumber = Math.max(...blogs.map(blog => blog.blogs))
+    if(maxNumber > 0) {
+        let blogsQuantity = blogs.map(blog => blog.blogs)
+        return blogs[blogsQuantity.indexOf(maxNumber)];
+    }else{
+        return {}
+    }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
