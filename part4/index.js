b2c4767f1a5a7b102df548424caa8dcfa06b4c50
caren1 <czarnocki.w@wp.pl>
2020-06-22 15:54:22 +0200

    Added controler for blogs

--- part4/index.js
+++ part4/index.js
@@ -1,13 +1,13 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+// const http = require('http')
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
 
 
 // const app = require('./app') // <- actual Express application
-// const http = require('http')
-// const config = require('./utils/config')
+const http = require('http')
+const config = require('./utils/config')
 // const logger = require('./utils/logger')
 // const server = http.createServer(app);
 
@@ -24,7 +24,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(result => {
@@ -37,25 +37,6 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
