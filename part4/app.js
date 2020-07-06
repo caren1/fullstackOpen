bee55e7c158f18825ed40b1e9856886cd096115a
caren1 <czarnocki.w@wp.pl>
2020-07-06 13:30:29 +0200

    4.18 - bloglist expansion, step6

--- part4/app.js
+++ part4/app.js
@@ -3,11 +3,15 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -29,6 +33,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part4/models/user.js
+++ part4/models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true, minlength:3 },
   name: String,
-  paswordHash: { type: String },
+  passwordHash: String,
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
@@ -13,16 +13,17 @@ const userSchema = new mongoose.Schema({
   ],
 })
 
-userSchema.plugin(uniqueValidator)
-
 userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    delete returnedObject.paswordHash
+    delete returnedObject.passwordHash
   }
 })
 
+userSchema.plugin(uniqueValidator)
+
 const User = mongoose.model('User', userSchema)
+
 module.exports = User
