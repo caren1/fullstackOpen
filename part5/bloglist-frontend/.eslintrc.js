4a3b66b0fbdc2b05c1e21b6949e09e5fb51b12ff
caren1 <czarnocki.w@wp.pl>
2020-07-29 18:51:43 +0200

    5.12 - bloglist frontend, step12

--- part5/bloglist-frontend/.eslintrc.js
+++ part5/bloglist-frontend/.eslintrc.js
@@ -1,39 +1,49 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es2020': true,
-        "jest": true,
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest/globals": true 
     },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 11
+    "extends": [ 
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
-    'rules': {
-        'indent': [
-            'error',
-            2
+    "plugins": [
+        "react", "jest"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            2  
         ],
-        'linebreak-style': [
-            'error',
-            'windows'
+        "linebreak-style": [
+            "error",
+            "unix"
         ],
-        'quotes': [
-            'error',
-            'single'
+        "quotes": [
+            "error",
+            "single"
         ],
-        'semi': [
-            'error',
-            'never'
+        "semi": [
+            "error",
+            "never"
         ],
-        'eqeqeq': 'error',
-        'no-trailing-spaces': 'error',
-        'object-curly-spacing': [
-            'error', 'always'
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
         ],
-        'arrow-spacing': [
-            'error', { 'before': true, 'after': true }
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
         ],
-        'no-console': 0
+        "no-console": 0,
+        "react/prop-types": 0
     }
-}
+  }
