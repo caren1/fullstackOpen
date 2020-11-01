011a82ad771c51131308e80d716145e0724660d0
caren1 <czarnocki.w@wp.pl>
2020-11-01 20:54:50 +0100

    9.3 - Command line

--- part9/body-mass-index/bmiCalculator.ts
+++ part9/body-mass-index/bmiCalculator.ts
@@ -1,7 +1,24 @@
+interface BmiValues {
+    value1: number,
+    value2: number
+}
+
+const parseArguments = (args: Array<string>) : BmiValues => {
+    if (args.length < 4 ) throw new Error('Not enough arguments');
+    if (args.length > 4 ) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
 const calculateBmi = (height: number, weight: number): string => {
     let bmi =  Math.floor((weight / (height * height)) * 10000);
-    console.log(bmi);
-    
 
     if (bmi < 15) {
         return 'Very severely underweight';
@@ -16,7 +33,12 @@ const calculateBmi = (height: number, weight: number): string => {
     } else if (bmi > 35) {
         return 'Severly Obese';
     }
-
 }
 
-console.log(calculateBmi(178, 75));
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    calculateBmi(value1, value2);
+    console.log(calculateBmi(value1, value2));
+} catch (e) {
+    console.log('Error, something unexpected happened, message: ', e.message);
+}
--- part9/body-mass-index/exerciseCalculator.ts
+++ part9/body-mass-index/exerciseCalculator.ts
@@ -1,11 +1,32 @@
 interface Result {
     periodLength: number,
-        trainingDays: number,
-        target: number,
-        average: number,
-        success: boolean,
-        rating: number,
-        ratingDescription: string,
+    trainingDays: number,
+    target: number,
+    average: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+}
+
+interface ExerciseValues {
+    target: number,
+    days: Array < number >
+}
+
+const parseExerciseArguments = (args: Array < string > ): ExerciseValues => {
+    if (args.length < 10) throw new Error('Not enough arguments');
+    if (args.length > 10) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        console.log(args[3]);
+
+        return {
+            target: Number(args[2]),
+            days: [Number(args[3]), Number(args[4]), Number(args[5]), Number(args[6]), Number(args[7]), Number(args[8]), Number(args[9])]
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
 }
 
 const calculateExercises = (days: Array < number > , target: number): Result => {
@@ -48,11 +69,18 @@ const calculateExercises = (days: Array < number > , target: number): Result => 
     }
 }
 
-let userTrainingHours = [3, 0, 2, 4.5, 0, 3, 1];
-let userTrainingHours2 = [2, 2, 2, 2, 1, 0, 1];
-let userTrainingHours3 = [8, 0, 8, 0, 8, 0, 8];
+// let userTrainingHours = [3, 0, 2, 4.5, 0, 3, 1];
+// let userTrainingHours2 = [2, 2, 2, 2, 1, 0, 1];
+// let userTrainingHours3 = [8, 0, 8, 0, 8, 0, 8];
 
-console.log(calculateExercises(userTrainingHours, 2));
-console.log(calculateExercises(userTrainingHours, 50));
-console.log(calculateExercises(userTrainingHours2, 1.6666666666666667));
-console.log(calculateExercises(userTrainingHours3, 1));
+// console.log(calculateExercises(userTrainingHours, 2));
+// console.log(calculateExercises(userTrainingHours, 50));
+// console.log(calculateExercises(userTrainingHours2, 1.6666666666666667));
+// console.log(calculateExercises(userTrainingHours3, 1));
+
+try {
+    const { target, days } = parseExerciseArguments(process.argv);
+    console.log(calculateExercises(days, target));
+} catch (e) {
+    console.log('Error, something unexpected happened, message: ', e.message);
+}
