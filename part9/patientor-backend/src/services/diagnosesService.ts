4710abfddb7027e2947e63a7e8b3934ca8903726
caren1 <czarnocki.w@wp.pl>
2020-11-05 15:32:41 +0100

    9.10 -  Patientor backend, step3

---
+++ part9/patientor-backend/src/services/diagnosesService.ts
@@ -0,0 +1,17 @@
+import diagnoseData from '../../data/diagnoses';
+import { Diagnose } from '../types';
+
+const diagnoses: Array<Diagnose> = diagnoseData;
+
+const getDiagnoses = (): Array<Diagnose> => {
+    return diagnoses;
+};
+
+const addDiagnose = () => {
+    return null;
+};
+
+export default {
+    getDiagnoses,
+    addDiagnose
+};
